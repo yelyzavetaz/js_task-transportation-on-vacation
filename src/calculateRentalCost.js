6952f4b9b56326e3ec803a1290acef186c065a01
Yelyzaveta Zhyrova <lizzzhirova@gmail.com>
2025-04-21 18:46:18 +0300

    clean up readability of the code

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -1,15 +1,17 @@
 function calculateRentalCost(days) {
-  const numberOfDays = days;
-  const dailyRentalCost = 40;
+  const RENT_PER_DAY = 40;
   const SHORT_TERM = 3;
   const LONG_TERM = 7;
   const SHORT_TERM_DISCOUNT = 20;
   const LONG_TERM_DISCOUNT = 50;
-  const totalRentalCost = numberOfDays * dailyRentalCost;
 
-  if (numberOfDays >= LONG_TERM) {
+  const totalRentalCost = days * RENT_PER_DAY;
+
+  if (days >= LONG_TERM) {
     return totalRentalCost - LONG_TERM_DISCOUNT;
-  } else if (numberOfDays >= SHORT_TERM) {
+  }
+
+  if (days >= SHORT_TERM) {
     return totalRentalCost - SHORT_TERM_DISCOUNT;
   }
 
